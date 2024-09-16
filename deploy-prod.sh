2e6cbba21f7779a9a18fb3bc4ba0afba441338dc
Jeff Rey <jeff.rey4973@gmail.com>
2024-09-16 22:53:22 +0200

    Bug fixes

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,16 +5,16 @@ USER="jeffrey"
 HOST="192.168.1.107"
 TARGET_DIR="/home/jeffrey/app"
 
-# Step 1: Copy Docker Compose files to the Ubuntu VM
+# Step 1: Copy files to the Ubuntu VM
 # scp -r ./. $USER@$HOST:$TARGET_DIR
 rsync -av --files-from=<(git ls-files) ./ $USER@$HOST:$TARGET_DIR
 
 # Step 2: SSH into the Ubuntu VM and build/run the containers
 ssh $USER@$HOST << EOF
   cd $TARGET_DIR
-  export DJANGO_ENV=production
+  export DJANGO_ENV=test
   docker compose -f docker-compose.prod.yml build
-  docker compose up -d
+  docker-compose up -d
 EOF
 
 echo "Deployment complete."
