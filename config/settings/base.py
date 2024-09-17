f85fd278ab73989c7b81e1b6dc0d9cdba45c879b
Jeff Rey <jeff.rey4973@gmail.com>
2024-09-17 22:39:41 +0200

    Added sites to the installed apps so we can change the display name in our gmail email

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd party apps
     'django_celery_results',
